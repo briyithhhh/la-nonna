2e580bdbaf09ebc5b9af8fb6fc283b62370f6c58
Briyith <portillobriyith2@gmail.com>
2022-10-21 22:36:53 -0400

    Nonna_frontend

---
+++ work/urbe/la-nonna/js/validations.js
@@ -0,0 +1,15 @@
+(function ($) {
+  "use strict";
+  var nonna = {
+    initiliased: false,
+    version: 'nonna',
+    mobile: false,
+    init: function () {
+      if (!this.initiliased) {
+        
+      }
+    }
+  }
+}
+
+)
