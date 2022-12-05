ca7dad1a6dd63ce8584a741c2cab64fbbf52ba0b
Briyith <portillobriyith2@gmail.com>
2022-12-05 13:22:49 -0400

    first

--- work/urbe/la-nonna/js/validations.js
+++ work/urbe/la-nonna/js/validations.js
@@ -1,15 +1,125 @@
-(function ($) {
-  "use strict";
-  var nonna = {
-    initiliased: false,
-    version: 'nonna',
-    mobile: false,
-    init: function () {
-      if (!this.initiliased) {
-        
-      }
-    }
-  }
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+const respuesta = document.querySelector('#respuesta');
+const btn = document.querySelector('#btn');
+
+const expresiones = {
+	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ ,// 7 a 14 numeros.
 }
 
-)
+const campos = {
+	apellido: false,
+	nombre: false,
+	correo: false,
+	telefono: false,
+}
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "apellido":
+			validarCampo(expresiones.apellido, e.target, 'apellido');
+		break;
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, 'telefono');
+		break;
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+
+	
+}
+
+
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const terminos = document.getElementById('terminos');
+	if(campos.apellido && campos.nombre  && campos.correo && campos.telefono &&  terminos.checked ){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+		window.location.href = "https://briyithhhh.github.io/la-nonna/enviar.html";
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
+
+	btn.addEventListener('click', (event) => {
+		event.preventDefault();
+		postData();
+		
+	  })
+});
+
+const getData = () => {
+	const datos = new FormData(formulario);
+	const datosProcesados = Object.fromEntries(datos.entries());
+	formulario.reset();
+	return datosProcesados;
+  }
+
+  const postData = async () => {
+ 
+	/*Crea un objeto con la informacion del formulario*/
+	 const newUser = getData();
+  
+	 try{
+	  const response = await fetch('http://localhost:3000/users', {
+	  method: 'POST',
+	  headers: {'Content-Type': 'application/json'},
+	  body: JSON.stringify(newUser)
+	  });
+	  
+  
+	  if(response.ok){
+		  const jsonResponse = await response.json();
+  
+  
+		  const {nombre,apellido,correo,telefono,mensaje} = jsonResponse;
+  
+		 
+	  }
+	 
+	 }catch(error){
+	   console.log(error);
+	 }
+	 
+  }
+  
